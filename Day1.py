e379197221f04386721a191582e84947adcc4d7a
Sreejaramgari <sreejaramgari028@gmail.com>
2026-02-12 15:41:18 +0530

    Python Language

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("This is Sreeja Ramgari")
+print("Today i have learnt about Introduction To python")
+print("I have learnt about Python Features")
+print("Python Programming is a Basic Fundamental Language")
---
+++ day 1 Intro to python.py
@@ -0,0 +1,3 @@
+print("This is Sreeja Ramgari")
+print("Today i have learnt about Introduction To python")
+print("I have learnt about Python Features")
