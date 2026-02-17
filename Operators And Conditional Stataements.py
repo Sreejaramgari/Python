95998d8f82b725b185d83de763100fe64a3a289b
Sreejaramgari <sreejaramgari028@gmail.com>
2026-02-18 00:24:10 +0530

    Add files via upload

---
+++ Operators And Conditional Stataements.py
@@ -0,0 +1,32 @@
+#Check if a number is zero
+num = int(input("Enter a number: "))
+if(num == 0):
+    print("num is zero")
+print("Program done")
+
+#check if number is zero or not
+num = int(input("Enter a number: "))
+if(num == 0):
+    print("number is zero")
+else:
+    print("number is not zero")
+
+
+"""Check if number is not zero
+num = int(input("Enter a number: "))
+if(num != 0):
+    print("num is not zero")
+else:
+    print("num is zero")"""
+
+#check which number is bigger
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+if(a>b):
+    print("a is the bigger number")
+else:
+    print("b is bigger number")
+if(a<b):
+    print("a is smaller number")
+else:
+    print("b is smaller number")
