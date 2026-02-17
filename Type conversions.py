7fb903a1b19d1f731741712ca1b16331ebbf4a9a
Sreejaramgari <sreejaramgari028@gmail.com>
2026-02-18 00:16:37 +0530

    Add files via upload

---
+++ Type conversions.py
@@ -0,0 +1,29 @@
+name=input("Enter Your name :")
+#implicit type conversion
+age=int(input("enter your age :"))
+age=age+1.0
+a = 5      # int
+b = 2.5    # float
+c = a + b
+print(c)
+print(type(c))
+#Explicit type conversion
+x = "20"        # string
+y = 5
+z = int(x) + y
+print(z)
+name = "Sreeja"
+age = 21
+print(type(name))
+print(type(age))
+print(f"your name is {name}")
+print(f"your age is {age}")
+
+#converting integers into float,string,bool
+num=12
+print(float(num))
+print(str(num))
+print(bool(num))
+
+
+
